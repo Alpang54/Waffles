3c2291765f3fd453682a1fc83173e0fa8f271d44
Alpang54 <alvis-ang@hotmail.com>
2019-09-09 22:00:30 +0800

    Login and Register without database access

---
+++ Waffles_project/Assets/User.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Collections;
+
+[Serializable]
+public class User
+{
+
+    private string email;
+    private string password;
+
+    public User(string email, string password)
+    {
+       this.email = email;
+       this.password = password;
+        
+    }
+
+    public string getEmail()
+    {
+
+        return email;
+    }
+
+    public string getPassword()
+    {
+        return password;
+    }
+
+
+}
