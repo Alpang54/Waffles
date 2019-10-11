3edea8819f2a2544a1d15900ff10fa227ada1604
Kai <darkenelfs@hotmail.com>
2019-10-11 20:32:27 +0800

    create custom alpha done w/o error check

---
+++ Waffles_project/Assets/Scripts/Custom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Custom
+{
+    private string correct;
+    private string stageName;
+    private string stageQuestion;
+    private string answer1;
+    private string answer2;
+    private string answer3;
+    private string answer4;
+
+
+    public Custom()
+    {
+
+    }
+
+    public Custom(string stageName , string stageQuestion , string answer1 , string answer2 , string answer3 , string answer4,string correct)
+    {
+        this.StageName = stageName;
+        this.StageQuestion = stageQuestion;
+        this.Answer1 = answer1;
+        this.Answer2 = answer2;
+        this.Answer3 = answer3;
+        this.Answer4 = answer4;
+        this.correct = correct;
+    }
+
+    public string StageName { get => stageName; set => stageName = value; }
+    public string StageQuestion { get => stageQuestion; set => stageQuestion = value; }
+    public string Answer1 { get => answer1; set => answer1 = value; }
+    public string Answer2 { get => answer2; set => answer2 = value; }
+    public string Answer3 { get => answer3; set => answer3 = value; }
+    public string Answer4 { get => answer4; set => answer4 = value; }
+    public string Correct { get => correct; set => correct = value; }
+}
---
+++ Waffles_project/Assets/Scripts/InitContent.cs
@@ -0,0 +1,50 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitContent : MonoBehaviour
+{
+    public GameObject extraContent;
+    public Text stageName;
+    public Transform contentPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DatabaseReference databaseRef;
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://cz3003-waffles.firebaseio.com/");
+        databaseRef = FirebaseDatabase.DefaultInstance.RootReference;
+        FirebaseDatabase.DefaultInstance.GetReference("CustomStage").GetValueAsync().ContinueWith(task => {
+          if (task.IsFaulted)
+          {
+              // Handle the error...
+          }
+          else if (task.IsCompleted)
+          {
+              DataSnapshot snapshot = task.Result;
+                
+              // Do something with snapshot...
+          }
+      });
+        for (int i=0;i<12;i++)
+        {
+            stageName.text = "Test" + i.ToString();
+            GameObject go = Instantiate(extraContent) as GameObject;
+            go.SetActive(true);
+            go.transform.SetParent(contentPanel);
+            
+            
+      
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
