94e108fb5811f24df6cf93b098600fc375ea0a0b
yeohjy <yeohjy@live.com.sg>
2019-10-22 11:31:33 +0800

    Added game map

---
+++ Waffles_project/Assets/Scripts/PlayScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayScreen : MonoBehaviour
+{
+	public string loadLevel;
+	public GameObject playWindow;
+	public GameObject nodeWindow;
+	public GameObject scoreWindow;
+	public int endFlag=0;
+	//public GameObject nextRow;
+	
+	public void openPlayWindow(int end=0){
+		playWindow.SetActive (true);
+		nodeWindow.SetActive (false);
+		endFlag = end;
+	}
+	
+	public void closePlayWindow(){
+		if (endFlag == 1)
+		{
+			endFlag = 0;
+			openScoreWindow();
+		}
+		playWindow.SetActive (false);
+		nodeWindow.SetActive (true);
+		
+	}
+
+	public void openScoreWindow()
+	{
+		playWindow.SetActive (false);
+		nodeWindow.SetActive (false);
+		scoreWindow.SetActive(true);
+	}
+
+	/*public void closeScoreWindow()//change scene instead
+	{
+		scoreWindow.SetActive(false);
+	}*/
+	
+}
