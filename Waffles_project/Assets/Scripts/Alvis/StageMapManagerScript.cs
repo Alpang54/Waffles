3ed4d5ddaa79963a36469bca5bfc3d41c64d3e60
WeiMin <weimin.mok@gmail.com>
2019-10-23 17:12:37 +0800

    Revert "Fixed linking from stage select to play"
    This reverts commit 4991ffc148e1449735894fda4900b5c5e5e5365c.

--- Waffles_project/Assets/Scripts/Alvis/StageMapManagerScript.cs
+++ Waffles_project/Assets/Scripts/Alvis/StageMapManagerScript.cs
@@ -82,8 +82,7 @@ public class StageMapManagerScript : MonoBehaviour
         
         Tuple<int, int> worldAndStageLevel = new Tuple<int, int>(this.worldLevel, stageLevel);
         datahandler.SetWorldAndStageLevel(worldAndStageLevel);
-        //SceneManager.LoadScene("Custom Lobby");
-        SceneManager.LoadScene("Game Map");
+        SceneManager.LoadScene("Custom Lobby");
     }
 
     public void ToggleDifficulty()
