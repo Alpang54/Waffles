ef66e3d973f994994573c7bab3e51a3a56395f58
ShuHF <54883383+ShuHF@users.noreply.github.com>
2019-10-31 23:58:57 +0800

    Added comments

--- Waffles_project/Assets/Scripts/CharacterSel.cs
+++ Waffles_project/Assets/Scripts/CharacterSel.cs
@@ -6,28 +6,39 @@ using UnityEngine.UI;
 
 public class CharacterSel : MonoBehaviour
 {
-    //figure out what is being selected
+    /// <summary>
+    /// This variable is to get the character(question diffcultiy) from the user input
+    /// </summary>
     private int selectedCharacterIndes;
 
     [Header("list of characters")]
-    //able to change in the inspector
     [SerializeField] private List<CharacterSelectObject> characterList = new List<CharacterSelectObject>();
 
-    // Start is called before the first frame update
-    //this is to reference in the unity 
+    /// <summary>
+    /// this is to reference in the unity 
+    /// </summary>
+     
     [Header("UI References")]
     [SerializeField] private Text description;
     [SerializeField] private Image characterimage;
     [SerializeField] private Text level;
     
+    /// <summary>
+    /// When the scene/page is loaded it will run this function
+    /// </summary>
 
     private void Start()
     {
-        
+        //hide the text
         GameObject.Find("Level").active = false;
+        //call function to change the output 
         UpdateCharacterSelectionUI();
     }
 
+    /// <summary>
+    /// When the user click on the left button this function will be activated
+    /// It will transverse the list by going left
+    /// </summary>
     public void LeftArrow()
     {
         selectedCharacterIndes--;
@@ -38,20 +49,30 @@ public class CharacterSel : MonoBehaviour
 
         UpdateCharacterSelectionUI();
     }
-
+    /// <summary>
+    /// When the user click on the right button this function will be activated
+    /// It will transverse the list by going right
+    /// </summary>
     public void RightArrow()
     {
         selectedCharacterIndes++;
+        //if the number is equal to the max character created
         if (selectedCharacterIndes == characterList.Count)
         {
+            //reset to 0
             selectedCharacterIndes = 0;
         }
 
         UpdateCharacterSelectionUI();
     }
 
+    /// <summary>
+    /// This function is for sending the question difficulty when the user click on the 
+    /// confirm (green button) 
+    /// </summary>
     public void ConfirmCharSel()
     {
+        
         //This is to set what the player has choosen for the diffculties of the game
         PlayerPrefs.SetInt("CharacterSelected" , int.Parse(level.text));
        //Test to get the value on what the level is in the console
@@ -59,8 +80,13 @@ public class CharacterSel : MonoBehaviour
     }
 
 
+    /// <summary>
+    /// This function is used to change the output(e.g image, description)
+    /// 
+    /// </summary>
     private void UpdateCharacterSelectionUI()
     {
+
         //change the viusal, description, image
         //change image
         characterimage.sprite =characterList[selectedCharacterIndes].splash;       
@@ -70,9 +96,14 @@ public class CharacterSel : MonoBehaviour
         level.text = characterList[selectedCharacterIndes].level;
     }
 
+    /// <summary>
+    /// This function is to set what the list contain 
+    /// </summary>
+
     [System.Serializable]
     public class CharacterSelectObject
     {
+
         //what does the character list contain
         //Character image
         public Sprite splash;
